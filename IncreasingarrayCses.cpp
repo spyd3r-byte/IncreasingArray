852081c1b99f7ae1673ab6c14d9279c8fc5f0bf2
Spyd3r <72187420+spyd3r-byte@users.noreply.github.com>
2020-10-17 13:36:29 +0530

    Create IncreasingarrayCses.cpp

---
+++ IncreasingarrayCses.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl '\n'
+#define rep(i,n) for (i = 0; i < n; i++) 
+using namespace std;
+ 
+int main()
+{
+     ll int n , count=0 , x=0;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+ 
+    for(int i=0;i<n-1;i++)
+    {
+        if(a[i]>a[i+1])
+        
+        
+    }
+    cout<<count;
+    return 0;
+}
